1ed23be6b47acf73f7a454437ae78e5229da8362
rushithorat <163113533+rushithorat@users.noreply.github.com>
2024-04-09 13:00:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var menubutton = document.querySelector('.Rushi');
+var midnav = document.querySelector('.mid');
+
+menubutton.addEventListener("click", function() {
+    midnav.classList.toggle('down');
+})
